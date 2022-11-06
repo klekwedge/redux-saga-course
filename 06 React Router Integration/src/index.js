81d4f19af8d668ef72b76dec59e0dc94ed083e5c
klekkwedge <antonov.andrey.10101@gmail.com>
2022-11-06 16:41:18 +0300

    feat: add 6 lesson

---
+++ 06 React Router Integration/src/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router';
+import { Route, Switch } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import Home from './pages/home/home';
+import LatestNews from './pages/latest-news/latest-news';
+import PopularNews from './pages/popular-news/popular-news';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import store from './redux/store';
+
+const history = createBrowserHistory();
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <Router history={history}>
+        <App>
+          <Switch>
+            <Route path='/' exact>
+              <Home />
+            </Route>
+            <Route path='/latest-news' exact>
+              <LatestNews />
+            </Route>
+            <Route path='/popular-news' exact>
+              <PopularNews />
+            </Route>
+          </Switch>
+        </App>
+      </Router>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
